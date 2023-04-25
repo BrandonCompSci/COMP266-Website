77daee8fb5aad16ab1b0d20130e31235d77db5a8
Brandon Gautier <brandoncompsci@protonmail.com>
2023-04-25 03:58:08 -0600

    Add light/dark toggle code

--- script.js
+++ script.js
@@ -3,6 +3,19 @@
 // w3schools.com. 2023. How To Toggle Between Dark and Light Mode. [online] Available at: 
 // <https://www.w3schools.com/howto/howto_js_toggle_dark_mode.asp> [Accessed 24 April 2023].
 function darkModeToggle() {
-    const toggleButton = document.body;
-    toggleButton.classList.toggle("light-mode");
+    // Declare constant variable 'body' and set it to 'body' element of document
+    const body = document.body;
+
+    // Declare contant variable 'toggleIcon' and select image element with ID 'toggle-icon'
+    const toggleIcon = document.getElementById('toggle-icon');
+    
+    // When button is clicked, toggle 'light-mode' settings on/off depending on whether class is present
+    body.classList.toggle("light-mode");
+
+    // If 'light-mode' class is present, display image of moon. If not, display image of sun
+    if (body.classList.contains('light-mode')) {
+        toggleIcon.src = './images/moon.png';
+    } else {
+        toggleIcon.src = './images/sun.png';
+    }
   }
