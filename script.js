5f1e3049eca0bb2934c19108ad7127ba9d930f84
Brandon Gautier <brandoncompsci@protonmail.com>
2023-04-24 18:10:52 -0600

    Add toggle button

---
+++ script.js
@@ -0,0 +1,8 @@
+// Implement Dark Mode / Light Mode toggle with JavaScript
+// Code sourced from: 
+// w3schools.com. 2023. How To Toggle Between Dark and Light Mode. [online] Available at: 
+// <https://www.w3schools.com/howto/howto_js_toggle_dark_mode.asp> [Accessed 24 April 2023].
+function darkModeToggle() {
+    const toggleButton = document.body;
+    toggleButton.classList.toggle("dark-mode");
+  }
