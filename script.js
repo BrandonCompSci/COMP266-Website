f24febf6dc5a7736e1e0009331be7661d4a0895d
Brandon Gautier <brandoncompsci@protonmail.com>
2023-05-04 12:00:07 -0600

    Add hamburger menu icon

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ function darkModeToggle() {
     } else {
         toggleIcon.src = './images/sun.png';
     }
-  }
+}
 
 // Implement a Scroll To Top Button
 // Code sourced from: 
@@ -45,4 +45,12 @@ function scrollFunction() {
 function topFunction() {
     document.body.scrollTop = 0; // For Safari
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+/**
+ * Implement Adaptive Top Navigation Menu
+ * 
+ */
+function adaptiveNav() {
+    
 }
